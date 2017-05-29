8b7387deb8edfe9151e5a093c3285bd1ca17a771
podengo <robertvphillips@gmail.com>
2017-05-29 12:46:56 -0400

    Ignore c:/* files, ie output files, in case I run local tests on unix and files of  names literally starting 'c:/' are generated in the git repo.

--- xml2rdb.py
+++ xml2rdb.py
@@ -19,66 +19,9 @@ import hashlib
 from lxml import etree
 from lxml.etree import tostring
 from pathlib import Path
+import elt
 
-''' Generic utility excape_xml_text:
-Given a str, replace embedded 'special xml characters' with their xml quotable formats.
-Also replace tabs with space for easier conversion of multiple fields later
-to tab-separated values.
-'''
-def escape_xml_text(str):
-    str = str.replace("<", "&lt;")
-    str = str.replace(">", "&gt;")
-    str = str.replace("&", "&amp;")
-    str = str.replace("\"", "&quot;")
-    str = str.replace("\t", " ")
-    return str
-
-'''Method add_subelements():
-Generic logging utility helper:
-
-Given an lxml element, add subelements recursively from nested python data structures
-This may be used to generate xml log files, however, it can take up too much core if used to
-report per-input-file messages, and if so, it may be
-better to disable for 'big' batches of xml files to convert, or break up to create multiple log files.
-'''
-def add_subelements(element, subelements, item_ids=False):
-    if isinstance(subelements, dict):
-        d_subelements = OrderedDict(sorted(subelements.items()))
-        for key, value in d_subelements.items():
-            # Check for valid xml tag name:
-            # http://stackoverflow.com/questions/2519845/how-to-check-if-string-is-a-valid-xml-element-name
-            # poor man's check: just prefix with Z if first character is a digit..
-            # the only bad type of tagname found ... so far ...
-            if key[0] >= '0' and key[0] <= '9':
-                key = 'Z' + key
-            try:
-                subelement = etree.SubElement(element, key)
-            except Exception as e:
-                print("Skipping etree.SubElement error='{}' for key='{}'"
-                     .format(e,key))
-                continue
-            add_subelements(subelement, value, item_ids=item_ids)
-    elif isinstance(subelements, list):
-        # Make a dict indexed by item index/count for each value in the 'value' that is a list
-        for i, value in enumerate(subelements):
-            id_filled = str(i+1).zfill(8)
-            if item_ids:
-                subelement = etree.Element("item")
-                subelement.attrib['id'] = id_filled
-                element.append(subelement)
-            else:
-                #encode the ID as a suffix to to element tag name itself
-                subelement = etree.SubElement(element, 'item-{}'.format(id_filled))
-
-            add_subelements(subelement, value, item_ids=item_ids)
-    else: # Assume it is a string-like value. Just set the element.text and do not recurse.
-        element.text = str(subelements)
-    return True
-# end def add_subelements()
-
-#
 #- - - - - - - - - - - START USER PARAMETERS
-#
 '''
     Note: I also envision a revision of this program to do an initial walk-through of the
     xml input files.
@@ -2590,7 +2533,7 @@ def xml2rdb(folders_base=None, input_path_list=None,
 
     e_root = etree.Element("uf-xml2rdb")
     # add_subelements_from_dict(e_root, d_log)
-    add_subelements(e_root, d_log)
+    elt.add_subelements(e_root, d_log)
 
     # log file output
     log_filename = '{}/log_xml2rdb.xml'.format(output_folder_secsz)
