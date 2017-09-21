68ab4dcb70892595eeadbf0e73a6befc5fdbeb32
Robert Vernon Phillips <podengo@ufl.edu>
2017-09-21 15:22:22 -0400

    Some Hathi_item fields added.

--- hathitrust/marshal1/marshal1/hathitrust/models.py
+++ hathitrust/marshal1/marshal1/hathitrust/models.py
@@ -1,9 +1,18 @@
 import uuid
 from django.db import models
+from django_enumfield import enum
 
 # Create your models here.
+class HathiItemState(enum.Enum):
+       HAS_FOLDER = 0
+       FOLDER_LOADED = 1
+       FILES_EXAMINED = 2
+       FILES_NEED_CHANGES = 3
+       YAML_CREATED = 4
+       YAML_
 
 class Hathi_item(models.Model):
     id = models.UUIDField(Primary_key=True, default=uuid.uuid4, editable=False)
     folder_path = models.CharField()
     state_code = models.IntegerField()
+    yaml_status = models.IntegerField()
