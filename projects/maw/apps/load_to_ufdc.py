f6afc4504f6e7585d93c1a2b74e690b413568b26
Robert Vernon Phillips <podengo@ufl.edu>
2018-02-27 08:46:05 -0500

    Progress on load_to_ufdc automatic uploader

---
+++ projects/maw/apps/load_to_ufdc.py
@@ -0,0 +1,9 @@
+'''
+Method test_loader()
+
+Tests an object that is set to load (copy) mets files from a
+a mounted staging directory to a ufdc load directory.
+At first, cron will call this method, and later a different
+scheduling system may be used to call this.
+
+'''
---
+++ projects/maw/apps/readme.txt
@@ -0,0 +1,3 @@
+Put maw apps under this folder.
+May soon split to subfolders like 'cli' for cli apps, etc,
+as a folder organization unfolds... rvp 20180227
