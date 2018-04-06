81a29e45689f412c8cd608ac97683dd95a1b25c9
Robert Vernon Phillips <podengo@ufl.edu>
2018-04-06 09:20:32 -0400

    Initial lcroyster files copied from HathiTrust.

--- projects/maw/marshal1/lcroyster/admin.py
+++ projects/maw/marshal1/lcroyster/admin.py
@@ -1,3 +1,147 @@
 from django.contrib import admin
+from .models import Item, File
+from .views import FormUploadFile
+from django.db import models
+from django.forms import TextInput, Textarea
 
-# Register your models here.
+class HathiModelAdmin(admin.ModelAdmin):
+    # Using should be a settings.py DATABASES key, a 'connection' name,
+    # actually, as called in misc Django messages
+    using = 'hathitrust_connection'
+
+    # Note, these overrides are for model fields withough explicit forms
+    # Where a form is defined, its form.CharField(..widget params...) widgets
+    # will NOT be overridden by these overrides.
+    formfield_overrides = {
+      models.CharField: { 'widget': TextInput( attrs={'size':'100'})},
+      models.TextField: { 'widget': Textarea( attrs={'rows':1, 'cols':'100'})},
+      }
+
+    #On admin change list page, show item name, not uuid(the default)
+    #list_display = ('item_name',)
+
+    def save_model(self, request, obj, form, change):
+        # Tell Django to save objects to the 'other' database.
+        obj.save(using=self.using)
+
+    def delete_model(self, request, obj):
+        # Tell Django to delete objects from the 'other' database
+        obj.delete(using=self.using)
+
+    def get_queryset(self, request):
+        # Tell Django to look for objects on the 'other' database.
+        return super().get_queryset(request).using(self.using)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        # Tell Django to populate ForeignKey widgets using a query
+        # on the 'other' database.
+        return super().formfield_for_foreignkey(db_field, request,
+            using=self.using, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        # Tell Django to populate ManyToMany widgets using a query
+        # on the 'other' database.
+        return super().formfield_for_manytomany(db_field, request,
+            using=self.using, **kwargs)
+
+    '''
+    From the django admin cookbook: method to delete an action from admin,
+    and in this case it is the 'delete_selected' action.
+    '''
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        action_to_delete = 'delete_selected'
+        if action_to_delete in actions:
+            del actions[action_to_delete]
+
+#end class HathiRouter
+
+class FileInline(admin.TabularInline):
+    model = File
+    show_change_link = True
+    extra = 0 # show no extra blank rows to enter data
+    fields = ('topic','content_type','size')
+    readonly_fields = ('content_type','size')
+
+    def has_add_permission(self,request, obj=None):
+        # With False, no 'add' button/link should appear for this inline table.
+        return True
+
+
+class ItemModelAdmin(HathiModelAdmin):
+    list_display = ['name', 'status', 'folder_path', 'modify_date',]
+    search_fields = ['name','status',]
+    inlines = (FileInline,)
+
+admin.site.register(Item, ItemModelAdmin)
+
+
+from django import forms
+
+
+class FileModelAdmin(HathiModelAdmin):
+  #{{{ custom field widget settings
+  def formfield_for_dbfield(self, db_field, **kwargs):
+    # Set form to upload form to use for adding...
+    form_field = super(FileModelAdmin, self).formfield_for_dbfield(db_field, **kwargs)
+    dw = form_field.widget.attrs
+
+    if db_field.name in ('topic', 'up_name', 'down_name', 'link_name', 'url'):
+       dw['class'] = 'special'
+       dw['size'] = '128'
+
+    return form_field
+  # end def formfield-for_dbfield
+
+  # form = FormUploadFile
+  # add_form = FormUploadFile
+  readonly_fields = ('date_time',)
+
+  # fields to display on the 'select' list
+  list_display = ('id','item','topic','content_type','size',)
+  search_fields = [ 'topic', 'upload_name' ]
+
+  list_filter = ['content_type',]
+  ordering = ['-id']
+
+  # fieldsets for edit-form display
+  fieldsets = (
+    (None, {
+      'fields': (  'topic', 'up_name',  'item', 'location')
+    }),
+
+    ('Details', {
+      'classes': ('collapse',),
+      'fields': ('url','down_name','content_type','charset','size', 'sha512',
+      'description',
+      'department','public','date_time'
+      )
+    }),
+    )
+    # end fieldsets
+
+  '''
+    More django admin cookbook tips: to delete add and delete buttons (as all
+    CubaLibro data should be imported?)
+    Remove the _exp method name suffix to implement.
+  '''
+  def has_add_permission(self,request, obj=None):
+        # With False, no 'add' button should appear per item
+        return True
+
+  # classes Meta, Media
+  class Meta:
+    ordering = ('-date_time', 'department',)
+
+  class Media:
+    js = ('jquery/jquery.js',
+          # 'wymeditor/jquery.wymeditor.js',
+          'admin_textarea.js')
+
+  # end classes Media, Meta
+# end class FileModelAdmin
+
+admin.site.register(File, FileModelAdmin)
+
+
+#admin.site.register(Upload, HathiModelAdmin)
--- projects/maw/marshal1/lcroyster/models.py
+++ projects/maw/marshal1/lcroyster/models.py
@@ -1,7 +1,12 @@
+import uuid
+import os
 from django.db import models
-
-# Create your models here.
 #from django_enumfield import enum
+
+#other useful model imports at times (see django docs, tutorials):
+import datetime
+from django.utils import timezone
+
 '''
 NOTE: rather than have a separate file router.py to host HathiRouter, I just
 put it here. Also see settings.py should include this dot-path
@@ -11,13 +16,17 @@ setting for DATABASE_ROUTERS.
 '''
 # Maybe move the HathiRouter later, but for now keep here
 #
-class OysterRouter:
+class HathiRouter:
     '''
     A router to control all db ops on models in the hathitrust Application.
     '''
 
-    app_label = 'uflib_oyster'
-    app_db = 'uflib_oyster_db'
+    # app_label is really an app name. Here it is hathitrust.
+    app_label = 'hathitrust'
+
+    # app_db is really a main settings.py DATABASES name, which is
+    # more properly a 'connection' name
+    app_db = 'hathitrust_connection'
 
     '''
     See: https://docs.djangoproject.com/en/2.0/topics/db/multi-db/
@@ -49,15 +58,211 @@ class OysterRouter:
 #end class
 
 # Create your models here.
-#class HathiItemState(enum.Enum):
+#
+# NB: we accept the django default of prefixing each real
+# db table name with the app_name, hence the model names
+# below do not start with hathi or hathitrust.
+# class HathiItemState(enum.Enum):
 #       HAS_FOLDER = 0
 #       FOLDER_LOADED = 1
 #       FILES_EXAMINED = 2
 #       FILES_NEED_CHANGES = 3
 #       YAML_CREATED = 4
 
-class Sensor_deployment(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    sensor_name = models.CharField(max_length=1024)
-    deployment_datetime = models.DateTimeField()
-    location_id = models.IntegerField()
+class Item(models.Model):
+
+    # Field 'id' is 'special', and if not defined, Django defines
+    # it as below.
+    # However I do include it per python Zen: explicit is better than implicit.
+    id = models.AutoField(primary_key=True)
+    #
+    uuid4 = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
+    name = models.CharField(max_length=255, unique=True,
+        default='Hathitrust item name')
+    modify_date = models.DateTimeField(auto_now=True)
+    folder_path = models.CharField(max_length=1024)
+
+    STATUS_CHOICES = (
+        ( 'new' ,'new'),
+        ( 'compiling','compiling'),
+        ( 'packaged','package is valid to send'),
+        ( 'sent','sent'),
+        ( 'evaluated','evaluated'),
+        ( 'recompiling','recompiling'),
+        ( 'done','done')
+    )
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+        default='new')
+
+    def __str__(self):
+        return self.name
+
+    ''' note: DO not set db_table. Let Django do its thing
+        and create the db table name via a prefix of the table
+        class of app_name and _. It makes
+        migrations and many management operations much easier down the line.
+        Changing it after doing some migrations will
+        confuse migrations, too, which can be somewhat messy, or require
+        a refresher review of migrations docs.
+
+        class Meta:
+          db_table = 'item'
+    '''
+
+#end class Item(models.Model)
+
+#end class Item
+
+class File(models.Model):
+  """
+  Each row in the model File describes a file and metadata for it to be
+  served by the Django webserver.
+
+  This is not populated manually, but rather via the upload method.
+
+  When a user uploads a file, the upload method uploads it and adds
+  a row in model File to indicate that the file is now uploaded onto
+  the webserver and available for linking via ../file/file_id, where
+  file_id is the id of the model File row.
+
+  The upload process physically puts the uploaded file to a file named
+  MEDIA_URL/file_id.ext, where .ext only appears where some extension
+  ".xxx" is given by the user inthe uploaded filename.
+
+  This assumes use of the usergroups application that requires a department
+  owner for each row of the model.
+
+  The file name is simply the id number, perhaps followed by the content-type
+  suffix, and it is stored in the host filesystem in MEDIA_URL directory.
+
+  The name_upload is the name used by the original uploader for the file,
+  saved to support a method that will re-download the file to the original
+  uploader so that this may be used as a backup.
+
+  The content_type is the purported content type by the uploader, possibly
+  to be validated. It is aka "mimetype", eg: "image/jpeg" or
+  "application/pdf", etc.
+
+  The sha5 hash is to uniquely identify the file contents.
+  This is useful to support an application to identify duplicate files
+  both within the Django site and among other hosts that may be
+  over-duplicating files and wasting space or retaining files too long.
+  """
+
+  # department owner of the uploaded file
+  department = models.CharField('Dept',max_length=64,default='RVP',db_index=True)
+  public     = models.BooleanField('Public',default=False)
+
+  #date_time the file row was added
+  date_time = models.DateTimeField('datetime',auto_now=True,db_index=True)
+
+  # Key words or topical info about the file contents, context
+  # It is meant to be a django-admin-searchable field
+  topic = models.CharField(max_length=128,db_index=True,blank=True,null=True)
+
+  item = models.ForeignKey('Item', on_delete=models.CASCADE,
+      db_index=True, blank=True, null=True)
+
+  description = models.TextField(null=True,blank=True)
+
+  #Create a subdirectory under media_root for the containing item of this file
+  #
+
+  # If you to change upload_to, make sure it is a writeable directory
+
+  location = models.FileField(upload_to="hathitrust/")
+
+  # NB: Must use hashlib module to make the hash re-calculable across
+  # operating systems, future releases of python, etc.
+  # sha512 is 512bits, hench 128 'hex chars' each representing 4 bits.
+  sha512 = models.CharField(max_length=128, db_index=True,null=True,blank=True)
+
+  up_name = models.CharField('up_name',max_length=128,default='tmpfile');
+
+  down_name= models.CharField('down_name',max_length=128,default='tmpfile',
+      null=True,blank=True);
+
+  link_name= models.CharField('link_name',max_length=128,default='click here',
+      null=True,blank=True);
+
+  # size of file in 8-bit bytes
+  size = models.IntegerField(default=0)
+
+  # uploadedfile objects - mirrored django1.1 attributes.
+  content_type = models.CharField('content_type',max_length=128,
+      default='text/plain')
+
+  #charset applies to 'text/*' content types.
+  charset = models.CharField('char_set',max_length=32,null=True,blank=True)
+
+  # Url is the reference url to use to download the file
+  url = models.CharField('url',max_length=256,default='tmpfile');
+
+  def __str__(self):
+        #2018
+        return "File"
+
+# end class File
+
+'''
+Legacy UploadFile object designed for more uses than is the leaner File model
+used by the Hathitrust app.
+
+If later will register with admin ... remember it has its own uploadform,
+so do not enable the Add permissions via admin, but admin views are OK.
+Can use the upload() method in views.py to invoke this if wanted.
+'''
+
+class UploadFile(models.Model):
+
+  # department owner of the uploaded file
+  department = models.CharField('Dept',max_length=64,default='RVP',db_index=True)
+  public     = models.BooleanField('Public',default=False)
+
+  #date_time the file row was added
+  date_time = models.DateTimeField('datetime',auto_now=True,db_index=True)
+
+  # Key words or topical info about the file contents, context
+  # It is meant to be a django-admin-searchable field
+  topic = models.CharField(max_length=128,db_index=True,blank=True,null=True)
+
+  item = models.ForeignKey('Item', on_delete=models.CASCADE,
+      db_index=True, blank=True, null=True)
+
+  description = models.TextField(null=True,blank=True)
+
+  # If you to change upload_to, make sure it is a writeable directory
+  location = models.FileField(upload_to="hathitrust/")
+
+  # NB: Must use hashlib module to make the hash re-calculable across
+  # operating systems, future releases of python, etc.
+  # sha512 is 512bits, hench 128 'hex chars' each representing 4 bits.
+  sha512 = models.CharField(max_length=128, db_index=True,null=True,blank=True)
+
+  up_name = models.CharField('up_name',max_length=128,default='tmpfile');
+
+  down_name= models.CharField('down_name',max_length=128,default='tmpfile',
+      null=True,blank=True);
+
+  link_name= models.CharField('link_name',max_length=128,default='click here',
+      null=True,blank=True);
+
+  # size of file in 8-bit bytes
+  size = models.IntegerField(default=0)
+
+  # uploadedfile objects - mirrored django1.1 attributes.
+  content_type = models.CharField('content_type',max_length=128,
+      default='text/plain')
+
+  #charset applies to 'text/*' content types.
+  charset = models.CharField('char_set',max_length=32,null=True,blank=True)
+
+  # Url is the reference url to use to download the file
+  url = models.CharField('url',max_length=256,default='tmpfile');
+
+  def __str__(self):
+        #2018
+        return "UploadFile"
+
+# end class UploadFile
