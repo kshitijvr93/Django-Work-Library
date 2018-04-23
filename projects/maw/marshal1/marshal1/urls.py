fab143f4b50fc18f538231e476c06e21149f94bf
Robert Vernon Phillips <robertvphillips@gmail.com>
2018-04-22 20:45:53 -0400

    save work

--- projects/maw/marshal1/marshal1/urls.py
+++ projects/maw/marshal1/marshal1/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.urls import include, path, re_path
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from . import views
 from django.conf.urls import url
 
 # Set admin site display title
@@ -49,7 +50,11 @@ urlpatterns = [
     #path('logout/', auth_views.logout, name='logout'),
 
     url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
+    path('successfully_logged_out/', views.successfully_logged_out),
+    url(r'^logout/$', auth_views.logout, name='logout',
+      #{'next_page':'login/'}
+      ),
+
     url(r'^oauth/', include('social_django.urls', namespace='social')),  # <--
     #url(r'^admin/', admin.site.urls),
 ]
---
+++ projects/maw/marshal1/marshal1/views.py
@@ -0,0 +1,9 @@
+def successfully_logged_out():
+
+    rv = '''<html><body>
+    <h2>Successfully logged out</h2>
+    <a href='/login'>Click to login again</a>
+    <h2>Fare well!</h2>
+    </html</body>
+    '''
+    return rv
