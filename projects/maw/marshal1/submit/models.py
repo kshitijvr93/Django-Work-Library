3e8ca7a982d85fa0e7851da88dd017d40250de5a
Robert Vernon Phillips <podengo@ufl.edu>
2018-06-05 15:20:00 -0400

    Startapp structure, to manage structures for xml and rdb.

--- projects/maw/marshal1/submit/models.py
+++ projects/maw/marshal1/submit/models.py
@@ -60,7 +60,6 @@ class SubmitRouter:
 
 # } end class SubmitRouter
 
-
 class TypeModel(models.Model):
     id = models.AutoField(primary_key=True)
     name = SpaceCharField(max_length=255,
@@ -72,7 +71,6 @@ class TypeModel(models.Model):
 
     def __str__(self):
             return '{}'.format(self.name)
-
 
 class MaterialType(TypeModel):
     pass
@@ -101,18 +99,13 @@ class xrconfig(models.Model):
     def __str__(self):
             return '{}'.format(self.name)
 
-
-class xrc_index(models.Model):
-    id = models.AutoField(primary_key=True)
-    # use a char field to hold comma-separated indexes for now
-    # ? check if Django has an arrayfield? or postgres.
-    #
-
-    pass
-
 class xrc_rel(models.Model):
-    # hirarchical relation for an xrconfig output relation
+    # hierarchical relation for an xrconfig output relation
     id = models.AutoField(primary_key=True)
+
+    order = models.IntegerField(default=1,
+     help_text="Relative order to output this xml tag within the parent tag.")
+
     xrconfig = models.ForeignKey('xrconfig', null=False, blank=False,
         on_delete=models.CASCADE,)
 
@@ -121,9 +114,20 @@ class xrc_rel(models.Model):
     parent = models.ForeignKey('self', null=True, blank=True,
         on_delete=models.CASCADE,)
 
+    min_occurs = models.IntegerField(null=False, default=True
+      ,help_text="Minimum rows required with this parent.")
+
+    max_occurs = models.IntegerField(null=False, default=True
+      ,help_text="Maximum rows required with this parent.")
+
     name = SpaceCharField(max_length=255,
         unique=True, blank=False, null=False, default='',
         help_text="Unique name for this output relation for this xrconfig."
+        , editable=True)
+
+    xml_tag = SpaceCharField(max_length=255,
+        unique=True, blank=False, null=False, default='',
+        help_text="XML Tag name for one of the items in this relation."
         , editable=True)
 
     def __str__(self):
@@ -133,16 +137,35 @@ class xrc_rel(models.Model):
         unique_together = ('xrconfig', 'name')
         ordering = ['xrconfig', 'name', ]
 
-
 class xrc_field(models.Model):
     # nodes for an xr config output field
     id = models.AutoField(primary_key=True)
+
     xrc_rel = models.ForeignKey('xrc_rel', null=False,
         blank=False,
         on_delete=models.CASCADE,)
+
     name = SpaceCharField(max_length=255,
         unique=True, blank=False, null=False, default='',
         help_text="Unique name for this field for this output relation."
+        , editable=True)
+
+    is_required = models.BooleanField(null=False, default=True
+      ,help_text="True means a value is required.")
+
+    default = SpaceCharField(max_length=255,
+        unique=True, blank=False, null=False, default='',
+        help_text="Default value for this field."
+        , editable=True)
+
+    # This field will be considered as an XML tag, else as an attribute.
+    is_xml_tag = models.BooleanField(null=False, default=True
+      ,help_text="True means the xml_name is an xml_tag. "
+      "False for an attribute name.")
+
+    xml_name = SpaceCharField(max_length=255,
+        unique=True, blank=False, null=False, default='',
+        help_text="XML tag or attribute name for this field."
         , editable=True)
 
     # Now the only type is string.. may want to adopt the types of fields
@@ -150,8 +173,9 @@ class xrc_field(models.Model):
     # value is not used yet...
     type = SpaceCharField(max_length=255,
         unique=True, blank=False, null=False, default='',
-        help_text="Notes on the type to cast from values in this string field."
-        , editable=True)
+        help_text="Notes on editing and casting a value of this string field "
+          "to a desired type",
+        editable=True)
 
     def __str__(self):
             return '{}:{}.{}'.format(self.xrc_rel.xrconfig,
@@ -161,10 +185,9 @@ class xrc_field(models.Model):
         unique_together = (('xrc_rel', 'name'))
         ordering = ['xrc_rel', 'name', ]
 
-# xrc_ionode used to generate a map for an xml2rdb conversion
-# of an xml input file to an xrconfig set of relations
-
 class xrc_ionode(models.Model):
+    # xrc_ionode planned to represent a a map for an xml2rdb conversion
+    # of an xml input file to an xrconfig set of relations
     # nodes for an xr config input/output map
     id = models.AutoField(primary_key=True)
     xrconfig = models.ForeignKey('xrconfig', null=False,
